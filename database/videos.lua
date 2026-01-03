6785948b0f5ccc675cd1a31d13a7be5d071d20d9
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 15:01:39 +0000

    Update videos.lua - Added 2 new entries

--- database/videos.lua
+++ database/videos.lua
@@ -1,3 +1,11 @@
 -- FiveManage Videos
 return {
+    ["Code_intro"] = {
+        label = "Code_intro",
+        video = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/videos/Code_intro.mp4"
+    },
+    ["mist_intro"] = {
+        label = "mist_intro",
+        video = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/videos/mist_intro.mp4"
+    }
 }
