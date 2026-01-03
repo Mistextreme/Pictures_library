5e799c466c8047808355774fb144f353fc348b82
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-01-03 14:58:46 +0000

    Update gifs.lua - Added 11 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -1,3 +1,47 @@
 -- FiveManage Gifs
 return {
+    ["77fd0b80-bcff-11f0-9239-e54eb8f772e3__1_"] = {
+        label = "77fd0b80-bcff-11f0-9239-e54eb8f772e3__1_",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/77fd0b80-bcff-11f0-9239-e54eb8f772e3%20(1).gif"
+    },
+    ["77fd0b80-bcff-11f0-9239-e54eb8f772e3"] = {
+        label = "77fd0b80-bcff-11f0-9239-e54eb8f772e3",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/77fd0b80-bcff-11f0-9239-e54eb8f772e3.gif"
+    },
+    ["Generated-File-November-06_-2025-6_46PM"] = {
+        label = "Generated-File-November-06_-2025-6_46PM",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/Generated-File-November-06_-2025-6_46PM.gif"
+    },
+    ["Generated-video-1"] = {
+        label = "Generated-video-1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/Generated-video-1.gif"
+    },
+    ["MistIntro"] = {
+        label = "MistIntro",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/MistIntro.gif"
+    },
+    ["a1690a90-668a-4b5e-84fb-83839dff6ff0_1"] = {
+        label = "a1690a90-668a-4b5e-84fb-83839dff6ff0_1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/a1690a90-668a-4b5e-84fb-83839dff6ff0_1.gif"
+    },
+    ["coding_intro__1_"] = {
+        label = "coding_intro__1_",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/coding_intro%20(1).gif"
+    },
+    ["coding_intro"] = {
+        label = "coding_intro",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/coding_intro.gif"
+    },
+    ["goat_coding__1_"] = {
+        label = "goat_coding__1_",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/goat_coding%20(1).gif"
+    },
+    ["goat_coding"] = {
+        label = "goat_coding",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/goat_coding.gif"
+    },
+    ["mistlogo"] = {
+        label = "mistlogo",
+        image = "https://raw.githubusercontent.com/Mistextreme/Pictures_library/main/Logos/mistlogo.gif"
+    }
 }
